54943e589d7bfa23a9b9e4973140a5107b0be372
Minjae Son <bungabear6422@gmail.com>
2026-02-27 11:10:27 +0900

    노션 설정 윈도우 착

---
+++ Views/NotionDatabaseSettingWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using NotionDeadlineFairy.ViewModels;
+using UserControl = System.Windows.Controls.UserControl;
+
+namespace NotionDeadlineFairy.Views
+{
+    /// <summary>
+    /// UserControl1.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class NotionDatabaseSettingWindow : UserControl
+    {
+        public NotionDatabaseSettingWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new NotionDatabaseSettingViewModel();
+        }
+    }
+}
